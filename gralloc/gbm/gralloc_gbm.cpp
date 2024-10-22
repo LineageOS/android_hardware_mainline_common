32117a5efb904ef3b65f9385c9f00669bbc2b14e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-23 05:39:05 +0800

    gbm_gralloc: Move property to vendor namespace
    To be compliant with Project Treble rules. Reference:
    https://source.android.com/docs/core/architecture/configuration/add-system-properties?hl=en#add-vendor-properties
    
    Change-Id: Iea1d9c7ba4db4593160335c501acc7ae87ad1672

--- gralloc/gbm/gralloc_gbm.cpp
+++ gralloc/gbm/gralloc_gbm.cpp
@@ -315,7 +315,7 @@ struct gbm_device *gbm_dev_create(void)
 	char path[PROPERTY_VALUE_MAX];
 	int fd;
 
-	property_get("gralloc.gbm.device", path, "/dev/dri/renderD128");
+	property_get("vendor.gralloc.gbm.device", path, "/dev/dri/renderD128");
 	fd = open(path, O_RDWR | O_CLOEXEC);
 	if (fd < 0) {
 		ALOGE("failed to open %s", path);
