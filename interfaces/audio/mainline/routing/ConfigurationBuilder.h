ebf782107f317e499d3ababbb65e1fabc3bef6fd
Aaron Kling <webgeek1234@gmail.com>
2026-09-07 02:51:59 -0500

    mainline/common: interfaces/audio/mainline: Support High Res Mix Port
    The primary output is expected to not have any high resolution audio and
    normally is just a 48k sample rate with a 16 bit pcm format. Higher
    quality should be in a separate mix port. Add such a mix port and
    separate the capabilities.
    
    Change-Id: Iea6accc258c43db3e890b3402f8f67b606d97205

--- interfaces/audio/mainline/routing/ConfigurationBuilder.h
+++ interfaces/audio/mainline/routing/ConfigurationBuilder.h
@@ -16,10 +16,13 @@ namespace aidl::android::hardware::audio::core::mainline::routing {
 
 // Names of the mix ports created by the builder.
 inline constexpr const char* kPrimaryOutputMixPort = "primary output";
+inline constexpr const char* kHiresOutputMixPort = "hra output";
 inline constexpr const char* kMultichannelOutputMixPort = "multichannel output";
 inline constexpr const char* kPrimaryInputMixPort = "primary input";
 inline constexpr const char* kUsbOutputMixPort = "usb output";
 inline constexpr const char* kUsbInputMixPort = "usb input";
+
+inline constexpr uint32_t kHraOutputCutoff = 88200;
 
 // Builds the Module::Configuration (device ports, mix ports, routes and
 // initial port configs) from the discovered endpoints and assigns the device
@@ -30,6 +33,8 @@ inline constexpr const char* kUsbInputMixPort = "usb input";
 //   * four USB device port templates (device / headset, in / out) served
 //     through connectExternalDevice();
 //   * "primary output" (PRIMARY flag) routed to every output device port;
+//   * "hra output" (DIRECT_PCM flag) routed to outputs that support high
+//     resolution audio;
 //   * "multichannel output" (DIRECT flag) routed to the outputs that accept
 //     six or more channels, only when such outputs exist;
 //   * "primary input" routed from every input device port;
